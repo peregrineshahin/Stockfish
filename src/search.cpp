f56397abd132a526c6a8ea42c3f7ab4e4cd3021c
peregrineshahin <peregrineshahin@gmail.com>
2022-10-23 13:42:07 +0300

    Increase reduction on cutnode based on 50 rule count
    Bench: 4295695

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 2;
+              r += pos.rule50_count();
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
