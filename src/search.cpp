3ef51acedfea5fda61e9a5130f94e789031c60dd
peregrineshahin <peregrineshahin@gmail.com>
2022-11-18 22:15:53 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1276,7 +1276,7 @@ moves_loop: // When in check, search starts here
                   alpha = value;
 
                   // Reduce other moves if we have found at least one score improvement
-                  if (   depth < 11
+                  if (   depth < 8
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                      depth = depth / 2 + 1;
