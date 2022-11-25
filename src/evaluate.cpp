3c54109720e3dcfb7934aa2ed22535b2b8f5649b
peregrineshahin <peregrineshahin@gmail.com>
2022-11-25 15:37:22 +0300

    Same bishop endgame (Adjudication disabled).
    bench: 4419306

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1082,6 +1082,11 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       optimism = optimism * (269 + nnueComplexity) / 256;
       v = (nnue * scale + optimism * (scale - 754)) / 1024;
+
+      if ((   more_than_one(pos.pieces( stm, BISHOP) & DarkSquares) || more_than_one(pos.pieces( stm, BISHOP) & ~DarkSquares)
+           || more_than_one(pos.pieces(~stm, BISHOP) & DarkSquares) || more_than_one(pos.pieces(~stm, BISHOP) & ~DarkSquares))
+           && pos.count<ALL_PIECES>() == 4)
+           v = Value(0);
   }
 
   // Damp down the evaluation linearly when shuffling
