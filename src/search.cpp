9cebf989f2abda6692f97dfe051b06bb6ad126db
peregrineshahin <peregrineshahin@gmail.com>
2022-11-28 11:08:47 +0300

    take 4 (clamp 0, 1594)
    bench: 4282114

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((12 * d + 282) * d - 349 , 1594);
+    return std::clamp((12 * d + 282) * d - 349, 0, 1594);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
