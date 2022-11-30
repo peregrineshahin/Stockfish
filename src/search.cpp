bb9f17d17cb04e173854f067fa1649abf52aa7f3
peregrineshahin <peregrineshahin@gmail.com>
2022-11-30 09:25:22 +0300

    Decrease reduction for Sacrifices in Lmr.
    bench: 4033372

--- src/search.cpp
+++ src/search.cpp
@@ -1156,10 +1156,10 @@ moves_loop: // When in check, search starts here
           if (singularQuietLMR)
               r--;
 
-          // Dicrease reduction if we move a threatened piece (~1 Elo)
+          // Decrease reduction if we move a threatened piece (~1 Elo)
           if (   depth > 9
               && (mp.threatenedPieces & from_sq(move)))
-              r--;
+              r -= 1 + (capture && abs(pos.psq_eg_stm()) > 1000);
 
           // Increase reduction if next ply has a lot of fail high
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
