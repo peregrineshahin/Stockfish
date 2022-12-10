b091f26ac6be56a3f233daf78b07c7af34956db4
peregrineshahin <peregrineshahin@gmail.com>
2022-12-10 21:42:29 +0300

    Revert "Update search.cpp"
    This reverts commit d15f12a0774920c50fb7046856ebe54d60da9757.

--- src/search.cpp
+++ src/search.cpp
@@ -1160,11 +1160,9 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Decrease reduction if we move a threatened piece (~1 Elo)
-          if ((mp.threatenedPieces & from_sq(move)))
-          {
-            r--;
-            dbg_mean_of(depth < 9);
-          }
+          if (   depth > 9
+              && (mp.threatenedPieces & from_sq(move)))
+              r--;
 
           // Increase reduction if next ply has a lot of fail high
           if ((ss+1)->cutoffCnt > 3)
