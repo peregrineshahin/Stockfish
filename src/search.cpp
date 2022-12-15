50092355925831c1354902aa47133414fabfc1e9
peregrineshahin <peregrineshahin@gmail.com>
2022-12-15 13:47:52 +0300

    Take 1
    bench: 3774850

--- src/search.cpp
+++ src/search.cpp
@@ -892,7 +892,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~4 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3;
+        depth -= 2 + ss->ttHit;
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
