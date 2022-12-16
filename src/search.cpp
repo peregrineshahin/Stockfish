fabac4eb7942eaacac8039d1e6cf7790b4d461f0
peregrineshahin <peregrineshahin@gmail.com>
2022-12-16 09:13:08 +0300

    Test the effect of using non pawn material in null move search.
    bench: 3530000

--- src/search.cpp
+++ src/search.cpp
@@ -794,7 +794,6 @@ namespace {
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 20 * depth - improvement / 13 + 233 + complexity / 25
         && !excludedMove
-        &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
     {
         assert(eval - beta >= 0);
