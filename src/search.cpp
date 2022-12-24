519029b62f580a14e9314b0131d95b1ac315d56f
peregrineshahin <peregrineshahin@gmail.com>
2022-12-24 21:04:04 +0300

    No double null move
    bench: 3623406

--- src/search.cpp
+++ src/search.cpp
@@ -788,6 +788,7 @@ namespace {
     // Step 9. Null move search with verification search (~35 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
+        && (ss-2)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 17139
         &&  eval >= beta
         &&  eval >= ss->staticEval
