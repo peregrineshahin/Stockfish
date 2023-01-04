53202d87f0854286465ded4f438d74dc2e5fb042
peregrineshahin <peregrineshahin@gmail.com>
2023-01-04 23:27:32 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1182,12 +1182,12 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
+          Depth d = std::clamp(newDepth - r, 1, newDepth + 1 + ss->doubleExtensions > 8);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           // Do full depth search when reduced LMR search fails high
-          if (value > alpha && d < newDepth)
+          if (value > alpha && d < newDepth - ss->doubleExtensions > 8)
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
