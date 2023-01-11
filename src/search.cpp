251bc426c833d52d9652d825a40f81bd08b1c7c8
peregrineshahin <peregrineshahin@gmail.com>
2023-01-11 20:05:26 +0300

    Sanity check of more reduction if we hit on all (r++)s
    bench: 3829232

--- src/search.cpp
+++ src/search.cpp
@@ -1160,6 +1160,9 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
+      if (((ss+1)->cutoffCnt > 3) && ttCapture && cutNode)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
