a1ae8714af70b1661951bb842e9e97ef54e9419e
peregrineshahin <peregrineshahin@gmail.com>
2023-01-14 14:10:25 +0300

    return High bound instead of soft on multiCut
    bench: 4068992

--- src/search.cpp
+++ src/search.cpp
@@ -1081,7 +1081,7 @@ moves_loop: // When in check, search starts here
               // that multiple moves fail high, and we can prune the whole subtree by returning
               // a soft bound.
               else if (singularBeta >= beta)
-                  return singularBeta;
+                  return value;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
