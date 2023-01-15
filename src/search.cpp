65044e4be009b856e233c31d3f52d55e1692891b
peregrineshahin <peregrineshahin@gmail.com>
2023-01-15 22:56:18 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1689,26 +1689,27 @@ moves_loop: // When in check, search starts here
     Piece moved_piece = pos.moved_piece(bestMove);
     PieceType captured = type_of(pos.piece_on(to_sq(bestMove)));
     int bonus1 = stat_bonus(depth + 1);
-
-    if (!pos.capture(bestMove))
+    int bonus2 = bestValue > beta + 146 ? bonus1               // larger bonus
+                                        : stat_bonus(depth);   // smaller bonus
+    
+    // Increase stats for the best move in case it was a capture move
+    if (pos.capture(bestMove))
     {
-        int bonus2 = bestValue > beta + 146 ? bonus1               // larger bonus
-                                            : stat_bonus(depth);   // smaller bonus
-
-        // Increase stats for the best move in case it was a quiet move
-        update_quiet_stats(pos, ss, bestMove, bonus2);
-
-        // Decrease stats for all non-best quiet moves
-        for (int i = 0; i < quietCount; ++i)
-        {
-            thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
-            update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
-        }
-    }
-    else
-        // Increase stats for the best move in case it was a capture move
         captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1;
+        bonus1 /= 6;
+        bonus2 /= 6;
+    }
+    // Increase stats for the best move in case it was a quiet move
+    update_quiet_stats(pos, ss, bestMove, bonus2);
 
+    // Decrease stats for all non-best quiet moves
+    for (int i = 0; i < quietCount; ++i)
+    {
+        thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
+        update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
+    }
+
+    
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
     if (   ((ss-1)->moveCount == 1 + (ss-1)->ttHit || ((ss-1)->currentMove == (ss-1)->killers[0]))
