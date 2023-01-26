a2ec5373914b940ca8445026c221f1d4775d11ea
peregrineshahin <peregrineshahin@gmail.com>
2023-01-26 21:17:19 +0300

    take 2
    bench: 3774637

--- src/search.cpp
+++ src/search.cpp
@@ -797,7 +797,7 @@ namespace {
         && (ss-1)->statScore < 18200
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 20 * depth - improvement / 14 + 235 + complexity / 24
+        &&  ss->staticEval >= beta - 20 * depth - improvement / 14 + 235 + complexity / 24 - std::abs(eval - complexity) / 52
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
