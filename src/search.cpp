f8105084847b503e3307f1a8876e36d567264ef7
peregrineshahin <peregrineshahin@gmail.com>
2023-01-26 21:17:19 +0300

    Revert "Also reduce pvNodes if ttMove"

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,10 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
-          r -= 1 + 11 / (3 + depth);
+          {
+            r -= 1 + 11 / (3 + depth);
+            dbg_mean_of(!ttMove);
+          }
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
