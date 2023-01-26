8d3f3f07bdfab72c55a8ae67ac92d454f0b4c55a
peregrineshahin <peregrineshahin@gmail.com>
2023-01-26 21:17:19 +0300

    Extract psq from eval and complexity for null move search
    bench: 4120047

--- src/search.cpp
+++ src/search.cpp
@@ -805,7 +805,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth, eval and complexity of position
-        Depth R = std::min(int(eval - beta) / 165, 6) + depth / 3 + 4 - (complexity > 800);
+        Depth R = std::min(int(eval - beta) / 165, 6) + depth / 3 + 4 - (complexity > 800) + (std::abs(eval - complexity) > 1780);
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
