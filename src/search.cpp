af873a4828fcb6275fa7297521ef368a84aa98a4
peregrineshahin <peregrineshahin@gmail.com>
2023-02-11 12:19:43 +0300

    Less reduction on all moves if givesCheck also.
    bench: 4714340

--- src/search.cpp
+++ src/search.cpp
@@ -1175,6 +1175,10 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
           r--;
 
+      if (   givesCheck
+          && abs(ss->staticEval) > 47)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
