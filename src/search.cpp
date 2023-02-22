968a6ae2f78189034a375646eef6ef61b7b9c8bf
peregrineshahin <peregrineshahin@gmail.com>
2023-02-22 22:03:51 +0300

    Disable check extension depth restriction if the check is by a queen.
    bench: 4296388

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop: // When in check, search starts here
 
           // Check extensions (~1 Elo)
           else if (   givesCheck
-                   && depth > 9
+                   && (depth > 9 || (type_of(pos.moved_piece(move)) == QUEEN))
                    && abs(ss->staticEval) > 78)
               extension = 1;
 
