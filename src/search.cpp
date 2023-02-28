a4677dcaa013cef42a2e834a1050cac3b67539fb
peregrineshahin <peregrineshahin@gmail.com>
2023-02-28 07:38:05 +0300

    50% TP experiment, it would be reasonable to at least try to decrease the culprit for non-linear scaling even more based on the status of the verification running and expect it gains elo in longer time control (1)
    bench: 5506504

--- src/search.cpp
+++ src/search.cpp
@@ -1071,7 +1071,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (2 + (ss->ttPv && !PvNode)) * depth;
+              Value singularBeta = ttValue - (1 + (ss->ttPv && !PvNode)) * depth;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
