bb1ca793b3e9e9bff5530be6cb4a5f6d9f317ce9
peregrineshahin <peregrineshahin@gmail.com>
2023-03-01 07:46:23 +0300

    Decrease reduction if move is an underpromotion and we have a good history
    bench: 4576743

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,10 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
           r--;
 
+      if (type_of(move) == PROMOTION && promotion_type(move) != QUEEN
+          && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
