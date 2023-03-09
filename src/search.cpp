7e0447ee75b0945b106303581e6feb14c44bdc83
peregrineshahin <peregrineshahin@gmail.com>
2023-03-09 06:58:05 +0300

    Take 3
    bench: 4876382

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if move is a killer and we have a good history
       if (move == ss->killers[0]
-          && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
+          && (*contHist[0])[movedPiece][to_sq(move)] >= 3722 * (depth > 6 && depth < 19))
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
