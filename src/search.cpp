d00aff851f973166247c94f0978218df084244ac
peregrineshahin <peregrineshahin@gmail.com>
2023-03-21 12:07:54 +0300

    Increase reduction if we are trying to find a move against the parent first killer while move picked has a bad history.
    bench: 5027597

--- src/search.cpp
+++ src/search.cpp
@@ -1181,9 +1181,13 @@ moves_loop: // When in check, search starts here
           r++;
 
       // Decrease reduction if move is a killer and we have a good history
-      if (move == ss->killers[0]
+      if (   move == ss->killers[0]
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
           r--;
+
+      if (   (ss-1)->currentMove == (ss-1)->killers[0]
+          && (*contHist[0])[movedPiece][to_sq(move)] < -3722)
+          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
