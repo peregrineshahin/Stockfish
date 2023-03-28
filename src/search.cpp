7ca583882834b0efc9d7447fb97953f400829213
peregrineshahin <peregrineshahin@gmail.com>
2023-03-28 08:12:15 +0300

    Take 3 extension for castling only.
    bench: 5062085

--- src/search.cpp
+++ src/search.cpp
@@ -1138,6 +1138,11 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
               extension = 1;
+
+          // Extension for Castling
+          else if (   type_of(move) == CASTLING
+                   && depth < 12)
+              extension = 1;
       }
 
       // Add extension to new depth
