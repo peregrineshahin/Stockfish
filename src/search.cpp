0f37fb6476dfc953f38bdc12e38006a92242b50e
peregrineshahin <peregrineshahin@gmail.com>
2023-04-17 18:05:37 +0300

    Test muzhen's PR guarding against verification search.
    Bench: 3334480

--- src/search.cpp
+++ src/search.cpp
@@ -824,7 +824,7 @@ namespace {
             if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14))
+            if (thisThread->nmpMinPly)
                 return nullValue;
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
