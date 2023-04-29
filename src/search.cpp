066894b44bc6700ccae1756960090bfba518805b
peregrineshahin <peregrineshahin@gmail.com>
2023-04-29 15:36:15 +0300

    Minimum depth of 2 for LMR.
    Bench: 6453175

--- src/search.cpp
+++ src/search.cpp
@@ -1194,7 +1194,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
+          Depth d = std::clamp(newDepth - r, 2, newDepth + 1);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
