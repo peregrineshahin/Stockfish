dae1cdd4fcaf39320c30484a2a58eaf34041bcfd
peregrineshahin <peregrineshahin@gmail.com>
2023-05-21 22:16:48 +0300

    Try deeper refinement also here.
    bench: 3235774

--- src/search.cpp
+++ src/search.cpp
@@ -1200,7 +1200,7 @@ moves_loop: // When in check, search starts here
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (bestValue + 63 + 11 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 662 && ss->doubleExtensions <= 6;
+              const bool doEvenDeeperSearch = value > bestValue + 662 && ss->doubleExtensions <= 6;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
