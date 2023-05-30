4276befa306b920ed64388aa876dd06aa57e73e0
peregrineshahin <peregrineshahin@gmail.com>
2023-05-30 14:32:43 +0300

    Try this to see if value actually matter or just at least beta.
    bench: 2555567

--- src/search.cpp
+++ src/search.cpp
@@ -801,8 +801,7 @@ namespace {
         if (nullValue >= beta)
         {
             // Do not return unproven mate or TB scores
-            if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
-                nullValue = beta;
+            nullValue = beta;
 
             if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14))
                 return nullValue;
