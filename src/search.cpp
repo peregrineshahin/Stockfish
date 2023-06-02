666f584ed5dacad144e7cf1e4a5e4349d70b5c64
peregrineshahin <peregrineshahin@gmail.com>
2023-06-02 11:51:51 +0300

    Fds Tweak.
    bench: 2617202

--- src/search.cpp
+++ src/search.cpp
@@ -1222,7 +1222,7 @@ moves_loop: // When in check, search starts here
           if (!ttMove && cutNode)
               r += 2;
 
-          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3) * !PvNode, !cutNode);
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
