397fa3751a89091faaef096a171105c793e0c75e
peregrineshahin <peregrineshahin@gmail.com>
2023-07-07 00:30:02 +0300

    Prevent very strange double qSearch Take 1
    bench: 2624928

--- src/search.cpp
+++ src/search.cpp
@@ -874,7 +874,7 @@ namespace {
                 value = -qsearch<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1);
 
                 // If the qsearch held, perform the regular search
-                if (value >= probCutBeta)
+                if (value >= probCutBeta && depth > 4)
                     value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
 
                 pos.undo_move(move);
