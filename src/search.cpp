fb9c53967bb28607a69a81a95cdf76e333d97fad
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-08-02 00:40:07 +0300

    Take 4
    bench: 1591522

--- src/search.cpp
+++ src/search.cpp
@@ -1117,6 +1117,9 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5168)
               extension = 1;
+
+          else if ((ss-1)->currentMove == MOVE_NULL && abs(ss->staticEval) > 420)
+              extension = 1;
       }
 
       // Add extension to new depth
