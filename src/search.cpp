5c65e63b9a4b88ec8bbbebb8b24018147b9cb616
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-08-20 15:05:18 +0300

    2 * !ttMove
    bench: 1555629

--- src/search.cpp
+++ src/search.cpp
@@ -1293,7 +1293,7 @@ moves_loop: // When in check, search starts here
 
               if (value >= beta)
               {
-                  ss->cutoffCnt += 1 + !ttMove;
+                  ss->cutoffCnt += 1 + 2 * !ttMove;
                   assert(value >= beta); // Fail high
                   break;
               }
