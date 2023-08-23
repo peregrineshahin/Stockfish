46f6fc45d9ea4b72285917596d45af03c82a0df2
peregrineshahin <peregrineshahin@gmail.com>
2023-08-23 10:18:04 +0300

    unclamped values in FDS deeper/shallower.
    bench: 1489270

--- src/search.cpp
+++ src/search.cpp
@@ -1187,9 +1187,9 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full-depth search based on LMR results - if the result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (bestValue + 64 + 11 * (newDepth - d));
+              const bool doDeeperSearch = value > (bestValue + 64 + 11 * r);
               const bool doEvenDeeperSearch = value > alpha + 711 && ss->doubleExtensions <= 6;
-              const bool doShallowerSearch = value < bestValue + newDepth;
+              const bool doShallowerSearch = value < bestValue + d + r;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
