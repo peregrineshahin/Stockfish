c46a0294359f2b9db77beac45c57f0409b6762d6
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-08-23 22:05:46 +0300

    See if allowing NMP is good in excluded search as the move is indeed excluded there (discussion on discord). - Rebased
    bench: 1526135

--- src/search.cpp
+++ src/search.cpp
@@ -779,7 +779,6 @@ namespace {
         &&  eval >= beta
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 21 * depth + 258
-        && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
         &&  beta > VALUE_TB_LOSS_IN_MAX_PLY)
