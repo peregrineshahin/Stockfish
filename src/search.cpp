4e29146256c92a2db13059daa765e9f52ce41f8f
peregrineshahin <peregrineshahin@gmail.com>
2023-08-25 17:13:21 +0300

    Take 3
    bench: 1531446

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
         && (ss-1)->statScore < 17329
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 21 * depth + 258
+        &&  ss->staticEval >= beta - 30 * depth + 258
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
@@ -1242,7 +1242,7 @@ moves_loop: // When in check, search starts here
       {
           RootMove& rm = *std::find(thisThread->rootMoves.begin(),
                                     thisThread->rootMoves.end(), move);
-
+ 
           rm.averageScore = rm.averageScore != -VALUE_INFINITE ? (2 * value + rm.averageScore) / 3 : value;
 
           // PV move or new best move?
@@ -1475,9 +1475,14 @@ moves_loop: // When in check, search starts here
                 bestValue = ttValue;
         }
         else
+        {
             // In case of null move search use previous static eval with a different sign
-            ss->staticEval = bestValue = (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
+            ss->staticEval = bestValue =      (ss-1)->currentMove != MOVE_NULL
+                                         ||  -(ss-1)->staticEval  >= beta ? evaluate(pos)
                                                                           : -(ss-1)->staticEval;
+        }
+
+
 
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
