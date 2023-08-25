0a18f5def7bcc7bba167244de1a5cc708c923bda
peregrineshahin <peregrineshahin@gmail.com>
2023-08-25 17:13:21 +0300

    Take 2
    bench: 1366136

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
         && (ss-1)->statScore < 17329
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 21 * depth + 258
+        &&  ss->staticEval >= beta - 30 * depth + 258
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
