042085cb72a15f171daaac78ea595e5598f6c30c
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-08-26 23:30:33 +0300

    Take 1
    Bench: 1398858

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,8 @@ moves_loop: // When in check, search starts here
           r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
+      if (    ttCapture
+          || (ttMove && moveCountPruning))
           r++;
 
       // Decrease reduction for PvNodes (~2 Elo)
