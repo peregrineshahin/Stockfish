914251ed1b31d141be0cfc540f062d7b1ce27151
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-08-28 18:16:41 +0300

    bench: 12555555

--- src/search.cpp
+++ src/search.cpp
@@ -1154,7 +1154,7 @@ moves_loop: // When in check, search starts here
           r++;
 
       else if (move == ttMove)
-          r--;
+          r--; 
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
