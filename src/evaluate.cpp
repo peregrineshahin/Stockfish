814b77d477804de6bead19745d519ec5e4302142
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-08-28 18:38:01 +0300

    Damp down captures and pawn pushes with a value of 6  instead of 0.
    bench: 1769286

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -163,7 +163,8 @@ Value Eval::evaluate(const Position& pos) {
        + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (200 - pos.rule50_count()) / 214;
+  int damping = pos.rule50_count() != 0 ? pos.rule50_count() : 6;
+  v = v * (200 - damping) / 214;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
