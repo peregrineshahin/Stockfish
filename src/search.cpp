169f831fb9d22e4a3065ac2e5ea94592174653b8
peregrineshahin <peregrineshahin@gmail.com>
2023-08-29 10:30:28 +0300

    flip order

--- src/search.cpp
+++ src/search.cpp
@@ -1150,11 +1150,11 @@ moves_loop: // When in check, search starts here
           r += 2;
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
-      if ((ss+1)->cutoffCnt > 3)
+      if (move == ttMove)
+          r--;
+      else if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      else if (move == ttMove)
-          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
