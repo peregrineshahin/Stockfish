07be8b99e79269dadf10966f1dc404bd27deaf15
peregrineshahin <peregrineshahin@gmail.com>
2023-08-30 10:41:23 +0300

    repetition
    bench: 1595889

--- src/search.cpp
+++ src/search.cpp
@@ -1145,9 +1145,8 @@ moves_loop: // When in check, search starts here
           r--;
       
       // Increase reduction on repetition (~1 Elo)
-      if (   move == (ss-4)->currentMove
-          && pos.has_repeated())
-          r += 2;
+      if (pos.has_repeated())
+          r += 1 + (move == (ss-4)->currentMove);
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
