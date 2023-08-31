e959f62488aff5d0dd6c35f8711bfe012b55d414
peregrineshahin <peregrineshahin@gmail.com>
2023-08-31 07:35:27 +0300

    No extensions on likelyFailLow.
    bench: 1461877

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,7 @@ moves_loop: // When in check, search starts here
 
       // Step 15. Extensions (~100 Elo)
       // We take care to not overdo to avoid search getting stuck.
-      if (ss->ply < thisThread->rootDepth * 2)
+      if (!likelyFailLow && ss->ply < thisThread->rootDepth * 2)
       {
           // Singular extension search (~94 Elo). If all moves but one fail low on a
           // search of (alpha-s, beta-s), and just one fails high on (alpha, beta),
