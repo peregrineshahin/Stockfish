05376c5fe875ad30359e6cd757dd3e15c92c4343
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-09-01 23:52:28 +0300

    Force usage of ttValue if previous move is a null move.
    bench: 1541865

--- src/search.cpp
+++ src/search.cpp
@@ -727,7 +727,7 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
-            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
+            && ((tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)) || (ss-1)->currentMove == MOVE_NULL))
             eval = ttValue;
     }
     else
