c3ae7b9c35951286b81a8db7a71d5e5a4ff0ecbc
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-09-03 11:22:32 +0300

    Eval tweak
    bench: 1478855

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -178,12 +178,12 @@ Value Eval::evaluate(const Position& pos) {
       nnue     -= nnue     * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
       int npm = pos.non_pawn_material() / 64;
-      v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
-           + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
+      v = (  nnue     * (925 + npm +     pos.count<PAWN>())
+           + optimism * (145 + npm + 9 * pos.count<PAWN>())) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (200 - shuffling) / 214;
+  v = v * (192 - shuffling) / 192;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
