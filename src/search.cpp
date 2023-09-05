957c67b3a18b5058c7aaac21f30f3320157e6a6d
peregrineshahin <peregrineshahin@gmail.com>
2023-09-05 09:30:29 +0300

    Take 2
    bench: 1508365

--- src/search.cpp
+++ src/search.cpp
@@ -776,7 +776,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
-        &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
+        &&  eval + (50 * !excludedMove) - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
