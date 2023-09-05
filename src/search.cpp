39ac8f3281611ab3172e0629b5eef7c744cf30ab
peregrineshahin <peregrineshahin@gmail.com>
2023-09-05 09:30:29 +0300

    Take 6 with Viz suggestion
    bench: 1538135

--- src/search.cpp
+++ src/search.cpp
@@ -72,8 +72,8 @@ namespace {
   enum NodeType { NonPV, PV, Root };
 
   // Futility margin
-  Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
-    return Value((140 - 40 * noTtCutNode) * (d - improving));
+  Value futility_margin(Depth d, bool noTtCutNode, bool improving, Move excludedMove) {
+    return Value((140 - 40 * noTtCutNode - 20 * !excludedMove) * (d - improving));
   }
 
   // Reductions lookup table initialized at startup
@@ -776,7 +776,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
-        &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
+        &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving, excludedMove) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
