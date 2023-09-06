70af1b239a8fa191c3135dc42ce1df4b89e5fac9
peregrineshahin <peregrineshahin@gmail.com>
2023-09-06 08:43:37 +0300

    Take 3 - 120
    bench: 1609885

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -159,10 +159,11 @@ Value Eval::evaluate(const Position& pos) {
   int shuffling  = pos.rule50_count();
   int simpleEval = simple_eval(pos, stm) + (int(pos.key() & 7) - 3);
 
-  bool lazy = abs(simpleEval) >=   RookValue + KnightValue
-                                 + 16 * shuffling * shuffling
-                                 + abs(pos.this_thread()->bestValue)
-                                 + abs(pos.this_thread()->rootSimpleEval);
+  bool lazy =     abs(pos.this_thread()->bestValue) > 120
+              && (abs(simpleEval) >=   RookValue + KnightValue
+                                     + 16 * shuffling * shuffling
+                                     + abs(pos.this_thread()->bestValue)
+                                     + abs(pos.this_thread()->rootSimpleEval));
 
   if (lazy)
       v = Value(simpleEval);
