7f235aa92d5f3bce03285b74057264f273288f9f
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-09-12 13:22:30 +0300

    Take 1 - 100k games
    bench: 1413307

--- src/search.cpp
+++ src/search.cpp
@@ -81,8 +81,8 @@ namespace {
 
   Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
     int reductionScale = Reductions[d] * Reductions[mn];
-    return  (reductionScale + 1372 - int(delta) * 1073 / int(rootDelta)) / 1024
-          + (!i && reductionScale > 936);
+    return  (reductionScale + 1272 - int(delta) * 759 / int(rootDelta)) / 1024
+          + (!i && reductionScale > 725);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
