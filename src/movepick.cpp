82dc205866171d86e651c351be3964ed2cb24c93
peregrineshahin <peregrineshahin@gmail.com>
2023-09-14 15:40:19 +0300

    take 3 (fixed)
    bench: 1542718

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -127,7 +127,7 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  (7 * int(PieceValue[pos.piece_on(to_sq(m))])
+          m.value = (7 * int(PieceValue[pos.piece_on(to_sq(m))]) -  2 * int(PieceValue[pos.piece_on(from_sq(m))]) / 3
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]) / 16;
 
       else if constexpr (Type == QUIETS)
