28881be8da7a302a49ddb08f71c55ada9f9fe256
peregrineshahin <peregrineshahin@gmail.com>
2023-09-19 09:24:45 +0300

    Take 1
    bench: 1526013

--- src/search.cpp
+++ src/search.cpp
@@ -901,7 +901,7 @@ namespace {
 moves_loop: // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
-    probCutBeta = beta + 413;
+    probCutBeta = beta + 45 * depth;
     if (   ss->inCheck
         && !PvNode
         && ttCapture
