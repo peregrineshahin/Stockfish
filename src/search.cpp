a7bc96cfaf70877a429d4c8a80fb93793c0411c3
peregrineshahin <peregrineshahin@gmail.com>
2023-09-21 07:16:15 +0300

    Retry BM idea that was simplified for some reason using 1 thread tests (??).
    bench: 1603079

--- src/search.cpp
+++ src/search.cpp
@@ -625,7 +625,7 @@ namespace {
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode
         && !excludedMove
-        && tte->depth() > depth
+        && tte->depth() > depth - (thisThread->id() % 2 == 1)
         && ttValue != VALUE_NONE // Possible in case of TT access race or if !ttHit
         && (tte->bound() & (ttValue >= beta ? BOUND_LOWER : BOUND_UPPER)))
     {
