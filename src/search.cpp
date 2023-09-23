3b5e426de3ccdb9f1a5266881c128df6bc6ca3e8
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-09-23 23:55:58 +0300

    See the effect of repetition on considering a ttPV.
    bench: 1383368

--- src/search.cpp
+++ src/search.cpp
@@ -620,7 +620,7 @@ namespace {
     // At this point, if excluded, skip straight to step 6, static eval. However,
     // to save indentation, we list the condition in all code between here and there.
     if (!excludedMove)
-        ss->ttPv = PvNode || (ss->ttHit && tte->is_pv());
+        ss->ttPv = PvNode || (ss->ttHit && tte->is_pv() && !pos.has_repeated());
 
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode
