528252ba19b1b66ad4d591608de56d528c6ab65b
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-09-30 18:01:27 +0300

    no likelyFailLow on exact bound
    bench: 1238930

--- src/search.cpp
+++ src/search.cpp
@@ -933,7 +933,7 @@ moves_loop: // When in check, search starts here
     // of this search was a fail low.
     bool likelyFailLow =    PvNode
                          && ttMove
-                         && (tte->bound() & BOUND_UPPER)
+                         && (tte->bound() == BOUND_UPPER)
                          && tte->depth() >= depth;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
