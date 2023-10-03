286cb65f341cf13ad62f006caa0116f7c25d4deb
peregrineshahin <peregrineshahin@gmail.com>
2023-10-03 16:22:12 +0300

    use rootDepth instead of adjusted depth in writing entries for root.
    bench: 1468187

--- src/search.cpp
+++ src/search.cpp
@@ -1385,7 +1385,7 @@ moves_loop: // When in check, search starts here
         tte->save(posKey, value_to_tt(bestValue, ss->ply), ss->ttPv,
                   bestValue >= beta ? BOUND_LOWER :
                   PvNode && bestMove ? BOUND_EXACT : BOUND_UPPER,
-                  depth, bestMove, ss->staticEval);
+                  rootNode ? thisThread->rootDepth : depth, bestMove, ss->staticEval);
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
 
