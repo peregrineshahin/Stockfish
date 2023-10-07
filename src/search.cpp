54a0e1c5a62b7f0536c8cf79a7dcd773e85f668b
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-10-07 16:17:38 +0300

    first passed patch

--- src/search.cpp
+++ src/search.cpp
@@ -766,7 +766,7 @@ namespace {
     // Step 7. Razoring (~1 Elo)
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (eval < alpha - 456 - 252 * depth * depth)
+    if (eval < alpha - 456 - (252 - 200 * ((ss+1)->cutoffCnt > 3)) * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
