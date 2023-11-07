b6348f3f6fffeb1df1144a7708f67d3435dbc294
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-11-08 00:08:24 +0300

    Ultra soft-bound simplification - return beta
    bench: 1304870

--- src/search.cpp
+++ src/search.cpp
@@ -885,9 +885,9 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
                 if (value >= probCutBeta)
                 {
                     // Save ProbCut data into transposition table
-                    tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3,
+                    tte->save(posKey, value_to_tt(beta, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3,
                               move, ss->staticEval);
-                    return value - (probCutBeta - beta);
+                    return beta;
                 }
             }
 
