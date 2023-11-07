0e0dc40129a147090ea344b3c4194a9ce0d76411
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-11-07 16:21:57 +0300

    Replace ttMove with ttValue != VALUE_NONE in likelyFailLow. probably more accurate?
    bench: 1114940

--- src/search.cpp
+++ src/search.cpp
@@ -922,7 +922,8 @@ moves_loop:  // When in check, search starts here
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal to or greater than the current depth, and the result
     // of this search was a fail low.
-    bool likelyFailLow = PvNode && ttMove && (tte->bound() & BOUND_UPPER) && tte->depth() >= depth;
+    bool likelyFailLow =
+      PvNode && ttValue != VALUE_NONE && (tte->bound() & BOUND_UPPER) && tte->depth() >= depth;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
