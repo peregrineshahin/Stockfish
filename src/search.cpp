a7eb2325fae4208070a50173c007a7e9563eced1
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-11-17 02:42:10 +0300

    probCutInCheck tweak
    bench: 1483302

--- src/search.cpp
+++ src/search.cpp
@@ -897,7 +897,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
-    probCutBeta = beta + 416;
+    probCutBeta = beta + 23 * depth;
     if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
         && tte->depth() >= depth - 4 && ttValue >= probCutBeta
         && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
