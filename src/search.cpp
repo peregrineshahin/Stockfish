974a91523f21e4f64184cf19eafc9798ac91ef21
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2023-11-19 21:21:33 +0300

    Double extensions for checks if they are the ttMove
    bench: 1504370

--- src/search.cpp
+++ src/search.cpp
@@ -1094,7 +1094,7 @@ moves_loop:  // When in check, search starts here
 
             // Check extensions (~1 Elo)
             else if (givesCheck && depth > 9)
-                extension = 1;
+                extension = 1 + (move == ttMove);
 
             // Quiet ttMove extensions (~1 Elo)
             else if (PvNode && move == ttMove && move == ss->killers[0]
