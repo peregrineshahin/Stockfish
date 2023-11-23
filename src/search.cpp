1d3af33bcc5db572d056220e15b3aaac3dd5b54d
peregrineshahin <peregrineshahin@gmail.com>
2023-11-23 16:43:12 +0300

    Take 6
    bench: 1429075

--- src/search.cpp
+++ src/search.cpp
@@ -1144,7 +1144,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if a quiet ttMove has been singularly extended (~1 Elo)
         if (singularQuietLMR)
-            r--;
+            r -= 2 * extension;
 
         // Increase reduction on repetition (~1 Elo)
         if (move == (ss - 4)->currentMove && pos.has_repeated())
