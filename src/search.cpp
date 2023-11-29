c532ce87e2d6ba4d5d0449b8488ea1d3c411ed1a
peregrineshahin <peregrineshahin@gmail.com>
2023-11-29 14:23:39 +0300

    Never reduction unless the move could have been pruned in the first place.
    bench: 1616748

--- src/search.cpp
+++ src/search.cpp
@@ -963,12 +963,14 @@ moves_loop:  // When in check, search starts here
 
         Value delta = beta - alpha;
 
-        Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
+        Depth r = 0;
 
         // Step 14. Pruning at shallow depth (~120 Elo).
         // Depth conditions are important for mate finding.
         if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
         {
+            r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
+
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
             if (!moveCountPruning)
                 moveCountPruning = moveCount >= futility_move_count(improving, depth);
