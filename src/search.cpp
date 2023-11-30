aff2240129aaaba5ff9e94e4846c923e07631ca9
peregrineshahin <peregrineshahin@gmail.com>
2023-11-30 09:54:06 +0300

    Doubtful this gains anything now let alone 5 Elo.
    bench: 1731533

--- src/search.cpp
+++ src/search.cpp
@@ -1150,13 +1150,8 @@ moves_loop:  // When in check, search starts here
         if (move == (ss - 4)->currentMove && pos.has_repeated())
             r += 2;
 
-        // Increase reduction if next ply has a lot of fail high (~5 Elo)
-        if ((ss + 1)->cutoffCnt > 3)
-            r++;
-
-        // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
-        // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
-        else if (move == ttMove)
+        // Nullifies all previous reduction adjustments to ttMove
+        if (move == ttMove)
             r = 0;
 
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
