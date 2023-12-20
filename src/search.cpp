ccdaedc941077fc03dd947e681132604634f61d6
peregrineshahin <peregrineshahin@gmail.com>
2023-12-20 15:39:40 +0300

    Take 2
    bench: 1114907

--- src/search.cpp
+++ src/search.cpp
@@ -1186,17 +1186,22 @@ moves_loop:  // When in check, search starts here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
             // Do a full-depth search when reduced LMR search fails high
-            if (value > alpha && d < newDepth)
+            if (value > alpha)
             {
-                // Adjust full-depth search based on LMR results - if the result
-                // was good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 53 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
+                if (d < newDepth)
+                {
+                    // Adjust full-depth search based on LMR results - if the result
+                    // was good enough search deeper, if it was bad enough search shallower.
+                    const bool doDeeperSearch =
+                      value > (bestValue + 53 + 2 * newDepth);                    // (~1 Elo)
+                    const bool doShallowerSearch = value < bestValue + newDepth;  // (~2 Elo)
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                    newDepth += doDeeperSearch - doShallowerSearch;
 
-                if (newDepth > d)
-                    value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
+                    if (newDepth > d)
+                        value =
+                          -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
+                }
 
                 int bonus = value <= alpha ? -stat_malus(newDepth)
                           : value >= beta  ? stat_bonus(newDepth)
