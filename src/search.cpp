ae143f40034dcd5374874eae0ca5a841943baf32
peregrineshahin <peregrineshahin@gmail.com>
2023-12-26 18:43:40 +0300

    dbg viz patch

--- src/search.cpp
+++ src/search.cpp
@@ -771,7 +771,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
-            return value;
+            return (value + alpha) / 2;
     }
 
     // Step 8. Futility pruning: child node (~40 Elo)
