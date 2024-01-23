506972e61ff2df5ab6799da9964fd6dfe96b920f
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-01-23 22:39:32 +0300

    fix misplaced window for years now in razoring
    bench: 1350045

--- src/search.cpp
+++ src/search.cpp
@@ -792,8 +792,8 @@ Value Search::Worker::search(
     // Adjust razor margin according to cutoffCnt. (~1 Elo)
     if (eval < alpha - 435 - (327 - 167 * ((ss + 1)->cutoffCnt > 3)) * depth * depth)
     {
-        value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
-        if (value < alpha)
+        value = qsearch<NonPV>(pos, ss, alpha, alpha+1);
+        if (value <= alpha)
             return value;
     }
 
