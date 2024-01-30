cc92dcd5906b7a38a1fd86c576eb2b2bc6b6cfe3
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-01-30 10:23:25 +0300

    Avoid queen capturing-check blunders
    bench: 1639208

--- src/search.cpp
+++ src/search.cpp
@@ -1527,8 +1527,8 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
         if (bestValue > VALUE_TB_LOSS_IN_MAX_PLY && pos.non_pawn_material(us))
         {
             // Futility pruning and moveCount pruning (~10 Elo)
-            if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
-                && move.type_of() != PROMOTION)
+            if ((!givesCheck || type_of(pos.moved_piece(move)) == QUEEN) && move.to_sq() != prevSq
+                && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY && move.type_of() != PROMOTION)
             {
                 if (moveCount > 2)
                     continue;
