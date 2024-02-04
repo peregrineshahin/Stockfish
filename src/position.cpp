196872658d2c7a0a064f24dc80631bcc9196fc20
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-02-04 20:00:39 +0300

    Update position.cpp

--- src/position.cpp
+++ src/position.cpp
@@ -669,7 +669,7 @@ void Position::do_move(Move m, StateInfo& newSt, bool givesCheck) {
     // Copy some fields of the old state to our new StateInfo object except the
     // ones which are going to be recalculated from scratch anyway and then switch
     // our state pointer to point to the new (ready to be updated) state.
-    std::memcpy(&newSt, st, offsetof(StateInfo, key));
+    std::memmove(&newSt, st, offsetof(StateInfo, key));
     newSt.previous = st;
     st             = &newSt;
 
