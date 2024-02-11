3c5dc9b5dc9587a5ec75667ae87e4835626d33ad
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-02-11 16:26:09 +0300

    [Rebased] Take 2
    bench: 1108228

--- src/search.cpp
+++ src/search.cpp
@@ -1254,7 +1254,8 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove;
+                    if (!PvNode)
+                        ss->cutoffCnt += 1 + !ttMove;
                     assert(value >= beta);  // Fail high
                     break;
                 }
