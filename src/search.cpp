e61238ad347c891beeac8883137b7cad61e9bbdd
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-02-19 15:00:26 +0300

    Take 2
    bench: 1513410

--- src/search.cpp
+++ src/search.cpp
@@ -825,7 +825,7 @@ Value Search::Worker::search(
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 181 - 68 * improving;
+    probCutBeta = beta + 181 + 154 * ((ss + 1)->cutoffCnt > 3) - 68 * improving;
     if (
       !PvNode && depth > 3
       && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
