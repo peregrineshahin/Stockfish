13bb75fe08226de6ec7d7ec3d98dfb8864a21c94
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-02-27 14:36:04 +0300

    Take 1
    bench: 1352863

--- src/search.cpp
+++ src/search.cpp
@@ -1120,7 +1120,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
-            r++;
+            r += r < 0 ? 2 : 1;
 
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
