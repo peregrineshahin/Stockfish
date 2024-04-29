5b41611c63c207b8f792d25916c81ba2dc3bb9ad
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-04-29 16:03:53 +0300

    Try basing TM on how many legal rootMove (fixed)
    bench: 1619613

--- src/search.cpp
+++ src/search.cpp
@@ -447,9 +447,10 @@ void Search::Worker::iterative_deepening() {
             double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
             int    el                  = std::clamp((bestValue + 750) / 150, 0, 9);
             double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;
+            double sizeFactor = std::clamp(double(5 * rootMoves.size()) / 100.00, 0.70, 1.00);
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
-                             * bestMoveInstability * EvalLevel[el] * recapture;
+                             * bestMoveInstability * EvalLevel[el] * recapture * sizeFactor;
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)
