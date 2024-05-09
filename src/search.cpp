cac51b5dc691c62ae5b4e6e9edcda801dae5c58e
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-05-09 08:14:41 +0300

    Just return ttValue and remove useless guards in such case.
    bench: 2976782

--- src/search.cpp
+++ src/search.cpp
@@ -892,9 +892,8 @@ moves_loop:  // When in check, search starts here
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
     probCutBeta = beta + 420;
     if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
-        && tte->depth() >= depth - 4 && ttValue >= probCutBeta
-        && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
-        return (beta + probCutBeta) / 2;
+        && tte->depth() >= depth - 4 && ttValue >= probCutBeta)
+        return ttValue;
 
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
