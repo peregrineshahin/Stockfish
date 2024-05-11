4b59865e7e1cafdaa2804ba500619230517027ba
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-05-11 21:21:49 +0300

    Take 2
    bench: 2491110

--- src/search.cpp
+++ src/search.cpp
@@ -820,7 +820,7 @@ Value Search::Worker::search(
 
     // Step 10. Internal iterative reductions (~9 Elo)
     // For PV nodes without a ttMove, we decrease depth by 3.
-    if (PvNode && !ttMove)
+    if (PvNode && !ttMove && (!ss->ttHit || tte->depth() + 4 > depth))
         depth -= 3;
 
     // Use qsearch if depth <= 0.
