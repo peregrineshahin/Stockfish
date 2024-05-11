0a44e4b1b27c339dbbc9c0479c6dd7be71e164e2
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-05-11 23:03:25 +0300

    Take 3
    bench: 2374403

--- src/search.cpp
+++ src/search.cpp
@@ -1285,7 +1285,7 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 13 && abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-                        depth -= 2;
+                        depth -= 2 + (!ttMove && depth > 4);
 
                     assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
