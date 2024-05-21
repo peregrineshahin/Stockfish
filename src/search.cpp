73a88b70c236939a0f46ce45cea4da46cf19abe0
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-05-21 12:25:10 +0300

    use ttCapture instead
    bench: 928868

--- src/search.cpp
+++ src/search.cpp
@@ -762,7 +762,7 @@ Value Search::Worker::search(
     // Step 7. Razoring (~1 Elo)
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (eval < alpha - 474 - 324 * depth * depth)
+    if (eval < alpha - 474 - (326 - 139 * ttCapture) * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
