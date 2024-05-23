a3805116524ec7b84d96237d84b1bfb93d259391
Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>
2024-05-23 23:44:21 +0300

    Take 5
    bench: 1533706

--- src/search.cpp
+++ src/search.cpp
@@ -776,7 +776,8 @@ Value Search::Worker::search(
                - (ss - 1)->statScore / 248
              >= beta
         && eval >= beta && eval < VALUE_TB_WIN_IN_MAX_PLY && (!ttMove || ttCapture))
-        return beta > VALUE_TB_LOSS_IN_MAX_PLY ? (eval + beta) / 2 : eval;
+        return beta > VALUE_TB_LOSS_IN_MAX_PLY && (ss + 1)->cutoffCnt <= 3 ? (eval + beta) / 2
+                                                                           : eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 13999
