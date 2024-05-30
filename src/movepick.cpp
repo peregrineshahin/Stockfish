b251e4cba0afac3c66415f6f3df7a1c5039f535c
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-05-30 12:35:06 +0300

    add promotions to Most Valuable Victim - fixed
    bench: 1459515

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -167,9 +167,11 @@ void MovePicker::score() {
     for (auto& m : *this)
         if constexpr (Type == CAPTURES)
             m.value =
-              7 * int(PieceValue[pos.piece_on(m.to_sq())])
+              7
+                * int(PieceValue[pos.piece_on(m.to_sq())] + (m.type_of() == PROMOTION
+                        ? PieceValue[m.promotion_type()]
+                        : 0))
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
-
         else if constexpr (Type == QUIETS)
         {
             Piece     pc   = pos.moved_piece(m);
