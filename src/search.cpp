06038b3e68dd5215caba61e62640bc70a7cb7a5b
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-06-01 18:14:41 +0300

    Take 4
    bench: 1270931

--- src/search.cpp
+++ src/search.cpp
@@ -131,7 +131,8 @@ void update_all_stats(const Position& pos,
                       int             quietCount,
                       Move*           capturesSearched,
                       int             captureCount,
-                      Depth           depth);
+                      Depth           depth,
+                      Move            ttMove);
 
 }  // namespace
 
@@ -1334,7 +1335,7 @@ moves_loop:  // When in check, search starts here
     // If there is a move that produces search value greater than alpha we update the stats of searched moves
     else if (bestMove)
         update_all_stats(pos, ss, *this, bestMove, bestValue, beta, prevSq, quietsSearched,
-                         quietCount, capturesSearched, captureCount, depth);
+                         quietCount, capturesSearched, captureCount, depth, ttMove);
 
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
@@ -1748,14 +1749,15 @@ void update_all_stats(const Position& pos,
                       int             quietCount,
                       Move*           capturesSearched,
                       int             captureCount,
-                      Depth           depth) {
+                      Depth           depth,
+                      Move            ttMove) {
 
     CapturePieceToHistory& captureHistory = workerThread.captureHistory;
     Piece                  moved_piece    = pos.moved_piece(bestMove);
     PieceType              captured;
 
     int quietMoveBonus = stat_bonus(depth + 1);
-    int quietMoveMalus = stat_malus(depth);
+    int quietMoveMalus = stat_malus(std::max(1, depth - (bestMove == ttMove)));
 
     if (!pos.capture_stage(bestMove))
     {
