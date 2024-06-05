bc802010c4fd67b4b4af88d8c3ea0980082fe8a1
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-06-05 15:13:20 +0300

    try this
    bench: 1431200

--- src/search.cpp
+++ src/search.cpp
@@ -1579,7 +1579,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
 
                 // If static exchange evaluation is much worse than what is needed to not
                 // fall below alpha we can prune this move.
-                if (futilityBase > alpha && !pos.see_ge(move, (alpha - futilityBase) * 4))
+                if (futilityBase >= beta && !pos.see_ge(move, (alpha - futilityBase) * 4))
                 {
                     bestValue = alpha;
                     continue;
