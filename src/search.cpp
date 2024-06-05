9a08797aa7d4d6e63744f33fe1c41373ef2014a2
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-06-05 16:11:02 +0300

    more probcut, account for possibility of exactness of ttValue (removed redundant condition)
    bench: 1172794

--- src/search.cpp
+++ src/search.cpp
@@ -855,7 +855,7 @@ Value Search::Worker::search(
       // there and in further interactions with transposition table cutoff depth is set to depth - 3
       // because probCut search has depth set to depth - 4 but we also do a move before it
       // So effective depth is equal to depth - 3
-      && !(tte->depth() >= depth - 3 && ttValue != VALUE_NONE && ttValue < probCutBeta))
+      && (tte->depth() < depth - 3 || ttValue >= beta))
     {
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
