1333dd791d31ddf6b6021659ea71cc50cd044287
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-07-02 02:17:13 +0300

    Try this big brain idea
    bench: 1151861

--- src/search.cpp
+++ src/search.cpp
@@ -1209,10 +1209,10 @@ moves_loop:  // When in check, search starts here
         {
             // Increase reduction if ttMove is not present (~6 Elo)
             if (!ttData.move)
-                r += 2;
+                r++;
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
-            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3), !cutNode);
+            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 2), !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
