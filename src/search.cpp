b683b1d5e00a2c7d15ee31a58541f0bd0ddba17f
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-07-06 12:25:54 +0300

    ttPv stuff
    bench: 1233483

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r--;
+            r -= 1 + (ss->ttHit && ttData.is_pv);
 
         // These reduction adjustments have no proven non-linear scaling.
 
