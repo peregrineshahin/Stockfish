b609c159c2f0fbf37170a8432a7e25c7ecce9ccf
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-07-19 16:36:40 +0300

    See if IIR is only beneficial for qsearch the dive.
    bench: 1626768

--- src/search.cpp
+++ src/search.cpp
@@ -833,11 +833,7 @@ Value Search::Worker::search(
 
     // Step 10. Internal iterative reductions (~9 Elo)
     // For PV nodes without a ttMove, we decrease depth.
-    if (PvNode && !ttData.move)
-        depth -= 3;
-
-    // Use qsearch if depth <= 0
-    if (depth <= 0)
+    if (PvNode && !ttData.move && depth < 4)
         return qsearch<PV>(pos, ss, alpha, beta);
 
     // For cutNodes, if depth is high enough, decrease depth by 2 if there is no ttMove,
