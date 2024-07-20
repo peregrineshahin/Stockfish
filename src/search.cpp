6e1842eb99b0570343e983c37e91803f68bb4ae0
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-07-20 23:43:00 +0300

    Take 2
    bench: 1633948

--- src/search.cpp
+++ src/search.cpp
@@ -1164,7 +1164,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
-            r += 1 + !(PvNode || cutNode);
+            r += 1 + !(PvNode || cutNode) + bool(excludedMove);
 
         // For first picked move (ttMove) reduce reduction, but never allow
         // reduction to go below 0 (~3 Elo)
