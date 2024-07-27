2c5345a880c52dc6e91e9b72613e3dc6780941c7
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-07-27 18:46:31 +0300

    use the cutoff count info into negative extensions
    bench: 1287716

--- src/search.cpp
+++ src/search.cpp
@@ -1087,6 +1087,8 @@ moves_loop:  // When in check, search starts here
                 // over current beta (~1 Elo)
                 else if (cutNode)
                     extension = -2;
+                else if (!PvNode && (ss + 1)->cutoffCnt > 4)
+                    extension = -2;
             }
 
             // Extension for capturing the previous moved piece (~1 Elo at LTC)
