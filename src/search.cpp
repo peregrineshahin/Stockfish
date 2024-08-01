072ecb3d33bf06a85b0d2a7f4a7599d53c66e625
peregrineshahin <41402573+peregrineshahin@users.noreply.github.com>
2024-08-01 16:29:10 +0300

    return instead of setting bestValue
    bench: 1593144

--- src/search.cpp
+++ src/search.cpp
@@ -1482,7 +1482,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             // ttValue can be used as a better position evaluation (~13 Elo)
             if (std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY
                 && (ttData.bound & (ttData.value > bestValue ? BOUND_LOWER : BOUND_UPPER)))
-                bestValue = ttData.value;
+                return ttData.value;
         }
         else
         {
