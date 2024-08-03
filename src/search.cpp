6698946eab78158eb50d2bfbeac00b02b4bcae79
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-08-03 10:43:58 +0300

    Take 4 - in cutoffCnt
    bench: 1218556

--- src/search.cpp
+++ src/search.cpp
@@ -1293,7 +1293,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttData.move - (extension >= 2);
+                    ss->cutoffCnt += 1 + (!ttData.move || ttData.depth <= 0) - (extension >= 2);
                     assert(value >= beta);  // Fail high
                     break;
                 }
