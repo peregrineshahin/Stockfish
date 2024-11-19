00646232222ae428108ad808f499921e0bcbb4c8
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-11-19 22:17:46 +0300

    Take 2
    bench: 969767

--- src/search.cpp
+++ src/search.cpp
@@ -774,8 +774,7 @@ Value Search::Worker::search(
     // false otherwise. The improving flag is used in various pruning heuristics.
     improving = ss->staticEval > (ss - 2)->staticEval;
 
-    opponentWorsening = ss->staticEval + (ss - 1)->staticEval > 2;
-
+    opponentWorsening = ss->staticEval + (ss - 1)->staticEval > 2 || ss->staticEval >= beta + 700;
     // Step 7. Razoring (~1 Elo)
     // If eval is really low, check with qsearch if we can exceed alpha. If the
     // search suggests we cannot exceed alpha, return a speculative fail low.
