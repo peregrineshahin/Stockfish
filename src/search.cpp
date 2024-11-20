0d0738a4a600cb54b48de1aa406d2126a91b7942
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-11-20 20:07:15 +0300

    Take 9
    bench: 755058

--- src/search.cpp
+++ src/search.cpp
@@ -1221,6 +1221,7 @@ moves_loop:  // When in check, search starts here
                 // Post LMR continuation history updates (~1 Elo)
                 int bonus = 2 * (value >= beta) * stat_bonus(newDepth);
                 update_continuation_histories(ss, movedPiece, move.to_sq(), bonus);
+                thisThread->mainHistory[us][move.from_to()] << 2 * bonus;
             }
         }
 
