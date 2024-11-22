a1dd8ed8d5309ce0f1b5f7d1ce1fd0887fae4c9a
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-11-22 16:49:53 +0300

    Take 1
    bench: 971670

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,8 @@ Value Search::Worker::search(
 
     // For cutNodes, if depth is high enough, decrease depth by 2 if there is no ttMove,
     // or by 1 if there is a ttMove with an upper bound.
-    if (cutNode && depth >= 7 && (!ttData.move || ttData.bound == BOUND_UPPER))
+    if (cutNode && depth >= 7
+        && !(ttData.move && (ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 6))
         depth -= 1 + !ttData.move;
 
     // Step 11. ProbCut (~10 Elo)
